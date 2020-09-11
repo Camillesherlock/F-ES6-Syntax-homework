8f525288e3b71265e552d39ee2936ceb680d80ab
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-11 21:44:59 +0800

    [lk] finish practice1 and practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
-export { parseData };
+const parseData = ({data,column}) => {
+  const keys = column.map(key => key.name)
+  const datas = [];
+  data.map(key => {
+      const values = {}
+      key.map((items,index) => {
+        values[keys[index]] = items
+      })
+      datas.push(values)
+  })
+   return datas;
+}
+
+export {parseData}
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,10 @@
-const inject
+const inject = (items, sections) => {
+    const contents = sections.map(key => key.content)
+    const index = sections.map(key => key.index)
+    contents.map((value,position) => {
+        console.log(value)
+        items.splice(index[position]+position,0,value);
+    })
+    return items;
+}
 export { inject };
