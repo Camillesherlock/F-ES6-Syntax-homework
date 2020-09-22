16af613f7452bd9965548dd1cb53326d42bf1fd5
kevindongzg <dongkevin94@gmail.com>
2020-09-22 14:39:16 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,5 +1,8 @@
+// TODO 可以思考一下可否用reduce来做
 const parseData = ({data,column}) => {
+  // TODO keys 没有必要
   const keys = column.map(key => key.name)
+  // TODO 使用map做数组与数组的转化，不用声明新数组
   const datas = [];
   data.map(key => {
       const values = {}
--- src/practice2.js
+++ src/practice2.js
@@ -1,6 +1,10 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
+    // TODO contents与index不是很有必要
     const contents = sections.map(key => key.content)
+    // TODO 不要使用index命名
     const index = sections.map(key => key.index)
+    // TODO 不是map的使用场景
     contents.map((value,position) => {
         console.log(value)
         items.splice(index[position]+position,0,value);
